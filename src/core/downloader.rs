4fb166253890051f96347d04a07f0326afb31391
b42-sneak <59374651+b42-sneak@users.noreply.github.com>
2020-08-10 01:17:16 +0200

    Tokio

--- src/core/downloader.rs
+++ src/core/downloader.rs
@@ -1,5 +1,13 @@
+use reqwest;
+use tokio;
+
 /// Downloads a comic given a URL and a destination
 pub fn download_from_url(url: &str, dest: &str, _verbosity: u64) -> () {
   println!("Destination: {}", dest);
   println!("URL: {}", url);
+
+  let mut rt = tokio::runtime::Runtime::new().unwrap();
+  let res = rt.block_on(reqwest::get(url));
+
+  println!("{:?}", res);
 }
