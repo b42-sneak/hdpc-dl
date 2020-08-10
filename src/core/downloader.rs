5459e9c49aa482704c1076ce945d82f093394a6a
b42-sneak <59374651+b42-sneak@users.noreply.github.com>
2020-08-10 03:27:11 +0200

    Selectors

--- src/core/downloader.rs
+++ src/core/downloader.rs
@@ -1,5 +1,7 @@
 use reqwest;
 use select::document::Document;
+// use select::predicate::{Attr};
+use scraper::{Html, Selector};
 
 /// Downloads a comic given a URL and a destination
 pub async fn download_from_url(
@@ -16,9 +18,30 @@ pub async fn download_from_url(
   let res = client.get(url).send().await?.text().await?.to_string();
 
   // println!("{:#?}", res);
-  let document = Document::from_read(res.as_bytes());
+  // let document = Document::from_read(res.as_bytes()).unwrap();
 
-  println!("{:#?}", document);
+  // println!("{:#?}", document);
+
+  // document.find(Attr())
+
+  let document = Html::parse_document(&res);
+
+  let artist_selector =
+    Selector::parse("#infoBox > div:nth-child(1) > span.pill-cube > a").unwrap();
+
+  let tags_selector = Selector::parse("#infoBox > div:nth-child(2) > span > a").unwrap();
+
+  let category_selector =
+    Selector::parse("#infoBox > div:nth-child(3) > span.pill-cube > a").unwrap();
+
+  let images_selector = Selector::parse("#infoBox > div:nth-child(4) > span.postImages").unwrap();
+
+  let rating_selector = Selector::parse("#infoBox > div:nth-child(6) > span.postLikes").unwrap();
+
+  // TODO Views, likes and dislikes are only available using another POST request
+
+  let date_selector = Selector::parse("#infoBox > div:nth-child(7) > span.postDate").unwrap();
+
   // This somehow makes this all work
   Ok(())
 }
